2358b16c1947eff67f0732754af6b3c4e1715ff9
Tom Sepez <tsepez@google.com>
2026-09-15 12:12:51 -0700

    [M154] Ignore re-entrant form calls during passive FFI callbacks
    Original change's description:
    > Ignore re-entrant form calls during passive FFI callbacks
    >
    > Passive FFI callbacks (e.g. FFI_OutputSelectedRect, FFI_Invalidate)
    > are notifications invoked during internal operations such as painting.
    > Embedders must not re-entrantly execute form actions or load/close
    > pages during these callbacks.
    >
    > Track passive callbacks using CPDFSDK_FormFillEnvironment's
    > in_passive_ffi_callback_ flag, and early-return from entry points
    > such as FORM_OnAfterLoadPage, FORM_OnBeforeClosePage, and
    > FORM_Do*Action when invoked re-entrantly.
    >
    > Active retrieval callbacks such as FFI_GetPage remain unflagged, as
    > page loading is expected in those paths.
    >
    > TAG=agy
    > CONV=9e87637a-cb01-4106-9c04-ad4da437fc61
    >
    > Bug: 556535630
    > Change-Id: Id892141ce082cc8895b4ed5dc66af3edaf5ef740
    > Reviewed-on: https://pdfium-review.googlesource.com/c/pdfium/+/156913
    > Reviewed-by: Lei Zhang <thestig@chromium.org>
    > Commit-Queue: Tom Sepez <tsepez@chromium.org>
    
    (cherry picked from commit 4a36628222bb647c23b4ef3b8f3fc64e93e1dd3b)
    
    Bug: 559037126,556535630
    Change-Id: Id892141ce082cc8895b4ed5dc66af3edaf5ef740
    Reviewed-on: https://pdfium-review.googlesource.com/c/pdfium/+/157650
    Bot-Commit: rubber-stamper@appspot.gserviceaccount.com <rubber-stamper@appspot.gserviceaccount.com>
    Auto-Submit: chrome-cherry-picker@chops-service-accounts.iam.gserviceaccount.com <chrome-cherry-picker@chops-service-accounts.iam.gserviceaccount.com>

--- fpdfsdk/cpdfsdk_formfillenvironment.cpp
+++ fpdfsdk/cpdfsdk_formfillenvironment.cpp
@@ -16,6 +16,7 @@
 #include "core/fpdfapi/parser/cpdf_array.h"
 #include "core/fpdfapi/parser/cpdf_dictionary.h"
 #include "core/fpdfdoc/cpdf_nametree.h"
+#include "core/fxcrt/autorestorer.h"
 #include "core/fxcrt/check.h"
 #include "core/fxcrt/containers/contains.h"
 #include "core/fxcrt/data_vector.h"
@@ -120,6 +121,9 @@ void CPDFSDK_FormFillEnvironment::OutputSelectedRect(
 
   CFX_PointF ptA = pFormField->PWLtoFFL(CFX_PointF(rect.left, rect.bottom));
   CFX_PointF ptB = pFormField->PWLtoFFL(CFX_PointF(rect.right, rect.top));
+
+  AutoRestorer<bool> ffi_restorer(&in_passive_ffi_callback_);
+  in_passive_ffi_callback_ = true;
   info_->FFI_OutputSelectedRect(info_, pPage, ptA.x, ptB.y, ptB.x, ptA.y);
 }
 
@@ -366,6 +370,8 @@ IJS_Runtime* CPDFSDK_FormFillEnvironment::GetIJSRuntime() {
 void CPDFSDK_FormFillEnvironment::Invalidate(IPDF_Page* page,
                                              const FX_RECT& rect) {
   if (info_ && info_->FFI_Invalidate) {
+    AutoRestorer<bool> ffi_restorer(&in_passive_ffi_callback_);
+    in_passive_ffi_callback_ = true;
     info_->FFI_Invalidate(info_, FPDFPageFromIPDFPage(page), rect.left,
                           rect.top, rect.right, rect.bottom);
   }
@@ -374,6 +380,8 @@ void CPDFSDK_FormFillEnvironment::Invalidate(IPDF_Page* page,
 void CPDFSDK_FormFillEnvironment::SetCursor(
     IPWL_FillerNotify::CursorStyle nCursorType) {
   if (info_ && info_->FFI_SetCursor) {
+    AutoRestorer<bool> ffi_restorer(&in_passive_ffi_callback_);
+    in_passive_ffi_callback_ = true;
     info_->FFI_SetCursor(info_, static_cast<int>(nCursorType));
   }
 }
@@ -394,6 +402,8 @@ void CPDFSDK_FormFillEnvironment::KillTimer(int nTimerID) {
 
 void CPDFSDK_FormFillEnvironment::OnChange() {
   if (info_ && info_->FFI_OnChange) {
+    AutoRestorer<bool> ffi_restorer(&in_passive_ffi_callback_);
+    in_passive_ffi_callback_ = true;
     info_->FFI_OnChange(info_);
   }
 }
@@ -416,6 +426,8 @@ void CPDFSDK_FormFillEnvironment::OnSetFieldInputFocusInternal(
     size_t nCharacters = text.GetLength();
     ByteString bsUTFText = text.ToUTF16LE();
     auto* pBuffer = reinterpret_cast<const unsigned short*>(bsUTFText.c_str());
+    AutoRestorer<bool> ffi_restorer(&in_passive_ffi_callback_);
+    in_passive_ffi_callback_ = true;
     info_->FFI_SetTextFieldFocus(
         info_, pBuffer, pdfium::checked_cast<FPDF_DWORD>(nCharacters), bFocus);
   }
@@ -484,6 +496,8 @@ void CPDFSDK_FormFillEnvironment::DisplayCaret(IPDF_Page* page,
                                                double right,
                                                double bottom) {
   if (info_ && info_->version >= 2 && info_->FFI_DisplayCaret) {
+    AutoRestorer<bool> ffi_restorer(&in_passive_ffi_callback_);
+    in_passive_ffi_callback_ = true;
     info_->FFI_DisplayCaret(info_, FPDFPageFromIPDFPage(page), bVisible, left,
                             top, right, bottom);
   }
@@ -901,6 +915,8 @@ void CPDFSDK_FormFillEnvironment::SendOnFocusChange(
   FPDF_ANNOTATION fpdf_annot =
       FPDFAnnotationFromCPDFAnnotContext(focused_annot.get());
 
+  AutoRestorer<bool> ffi_restorer(&in_passive_ffi_callback_);
+  in_passive_ffi_callback_ = true;
   info_->FFI_OnFocusChange(info_, fpdf_annot, pPageView->GetPageIndex());
 }
 
