1442e4fe069f058b19e7fd74fe154fee3c37d344
Tom Sepez <tsepez@google.com>
2026-09-18 16:50:15 -0700

    Add disabled embeddertests for form fill environment lifecycle crashes
    Consecutive FPDFDOC_InitFormFillEnvironment calls on an XFA document
    cause a null pointer dereference in CXFA_FFApp::GetHeap(), and closing
    an FPDF_DOCUMENT before exiting its form fill environment causes a
    heap-use-after-free in CPDFSDK_FormFillEnvironment.
    
    Neither of these are reachable without a misbehaving embedder.
    
    Bug: 563426171
    Change-Id: I6e5669e1ee8ae23158ba0d60a60d9ea062d3f4e0
    Reviewed-on: https://pdfium-review.googlesource.com/c/pdfium/+/158390
    Reviewed-by: Andy Phan <andyphan@chromium.org>
    Commit-Queue: Tom Sepez <tsepez@chromium.org>

---
+++ fpdfsdk/fpdfxfa/cpdfxfa_formfillenv_embeddertest.cpp
@@ -0,0 +1,68 @@
+// Copyright 2026 The PDFium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include <string>
+
+#include "fxjs/ijs_runtime.h"
+#include "public/fpdf_formfill.h"
+#include "public/fpdfview.h"
+#include "testing/gtest/include/gtest/gtest.h"
+#include "testing/utils/path_service.h"
+#include "testing/xfa_js_embedder_test.h"
+
+class CPDFXFAFormFillEnvEmbedderTest : public XFAJSEmbedderTest {};
+
+// TODO(crbug.com/563426171): Fix crash and re-enable.
+TEST_F(CPDFXFAFormFillEnvEmbedderTest, DISABLED_ReinitFormFillEnvironment) {
+  ASSERT_FALSE(IJS_Runtime::IsIsolatePerDocument());
+
+  std::string file_path = PathService::GetTestFilePath("simple_xfa.pdf");
+  ScopedFPDFDocument doc(FPDF_LoadDocument(file_path.c_str(), ""));
+  ASSERT_TRUE(doc);
+
+  IPDF_JSPLATFORM platform = {};
+  platform.version = 3;
+
+  FPDF_FORMFILLINFO formfillinfo = {};
+  formfillinfo.version = 2;
+  formfillinfo.m_pJsPlatform = &platform;
+
+  ScopedFPDFFormHandle form(
+      FPDFDOC_InitFormFillEnvironment(doc.get(), &formfillinfo));
+  ASSERT_TRUE(form);
+  EXPECT_TRUE(FPDF_LoadXFA(doc.get()));
+
+  form.reset();
+
+  form.reset(FPDFDOC_InitFormFillEnvironment(doc.get(), &formfillinfo));
+  ASSERT_TRUE(form);
+  EXPECT_TRUE(FPDF_LoadXFA(doc.get()));
+
+  ScopedFPDFPage page(FPDF_LoadPage(doc.get(), 0));
+  EXPECT_TRUE(page);
+}
+
+// TODO(crbug.com/563426171): Fix crash and re-enable.
+TEST_F(CPDFXFAFormFillEnvEmbedderTest, DISABLED_CloseDocumentBeforeExit) {
+  ASSERT_FALSE(IJS_Runtime::IsIsolatePerDocument());
+
+  std::string file_path = PathService::GetTestFilePath("simple_xfa.pdf");
+  ScopedFPDFDocument doc(FPDF_LoadDocument(file_path.c_str(), ""));
+  ASSERT_TRUE(doc);
+
+  IPDF_JSPLATFORM platform = {};
+  platform.version = 3;
+
+  FPDF_FORMFILLINFO formfillinfo = {};
+  formfillinfo.version = 2;
+  formfillinfo.m_pJsPlatform = &platform;
+
+  ScopedFPDFFormHandle form(
+      FPDFDOC_InitFormFillEnvironment(doc.get(), &formfillinfo));
+  ASSERT_TRUE(form);
+  EXPECT_TRUE(FPDF_LoadXFA(doc.get()));
+
+  doc.reset();
+  form.reset();
+}
